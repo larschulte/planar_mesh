1bc471441ec1bfe9690f6f97d0541cfdc05c38bd
jiahao <github@jiahao.wang>
2024-07-25 12:31:52 +0100

    split pose into double and unit3 to solve underdetermined system. improve code naming

--- src/utilities/gtsam_plane.cpp
+++ src/utilities/gtsam_plane.cpp
@@ -12,60 +12,72 @@
 #include <gtsam/nonlinear/Marginals.h>
 #include <gtsam/sam/BearingRangeFactor.h>
 
-class ProjectionFactor : public gtsam::NoiseModelFactor1<gtsam::Pose3>
+class ProjectionFactor : public gtsam::NoiseModelFactor2<double, gtsam::Unit3>
 {
 private:
-    Eigen::Vector3d o_; // origin
-    Eigen::Vector3d p_; // point
-    Eigen::Vector3d n_; // ray direction
-    double m_; // distance
+
+    // plane 
+    Eigen::Vector3d o_plane_; // plane origin
+    Eigen::Vector3d v_plane_; // plane direction
+
+    // point
+    Eigen::Vector3d o_point_; // point origin
+    Eigen::Vector3d p_point_; // point position
+    Eigen::Vector3d v_point_; // point direction
+    double t_point_; // point distance
 
 public:
-    ProjectionFactor(gtsam::Key j, Eigen::Vector3d o, Eigen::Vector3d p, const gtsam::SharedNoiseModel &model)
+    ProjectionFactor(gtsam::Key KEY_p_distance, gtsam::Key KEY_p_normal, Eigen::Vector3d o_plane, Eigen::Vector3d v_plane, Eigen::Vector3d o_point, Eigen::Vector3d p_point, const gtsam::SharedNoiseModel &model)
         : 
-        gtsam::NoiseModelFactor1<gtsam::Pose3>(model, j),
-        o_(o),
-        p_(p)
+        gtsam::NoiseModelFactor2<double, gtsam::Unit3>(model, KEY_p_distance, KEY_p_normal),
+        o_plane_(o_plane),
+        v_plane_(v_plane),
+        o_point_(o_point),
+        p_point_(p_point)
     {
-        n_ = (p - o).normalized();
-        m_ = (p - o).norm();
+        v_point_ = (p_point - o_point).normalized();
+        t_point_ = (p_point - o_point).norm();
     }
 
-    gtsam::Vector evaluateError(const gtsam::Pose3 &q, boost::optional<gtsam::Matrix &> H = boost::none) const override
+    gtsam::Vector evaluateError(const double& _t_plane, const gtsam::Unit3& _n_plane , boost::optional<gtsam::Matrix &> H_distance = boost::none, boost::optional<gtsam::Matrix &> H_normal = boost::none) const override
     {
-        // position and normal of the unkown pose
-        Eigen::Vector3d p0 = q.translation();
-        Eigen::Vector3d n0 = q.rotation().matrix().col(2);
-
         // h(q)
-        double h = n0.dot(p0 - o_) / n0.dot(n_);
-
-        // Optional: compute the Jacobian
-        if (H)
-        {
-            Eigen::MatrixXd H_p(1, 3); // Partial derivative w.r.t. translation
-            Eigen::MatrixXd H_R(1, 3); // Partial derivative w.r.t. rotation
-
-            // Compute partial derivative w.r.t. translation
-            H_p = n0.transpose() / n0.dot(n_);
-
-            // Compute partial derivative w.r.t. rotation
-            Eigen::Matrix3d skew_n0 = gtsam::skewSymmetric(n0);
-            H_R = ((p0 - o_).transpose() * skew_n0) / n0.dot(n_);
-
-            // Combine into a single Jacobian matrix
-            H->resize(1, 6);
-            H->block<1, 3>(0, 0) = H_p;
-            H->block<1, 3>(0, 3) = H_R;
-        }
+        Eigen::Vector3d n_plane = _n_plane.unitVector();
+        Eigen::Vector3d p_position = o_plane_ + v_plane_ * _t_plane;
+        double h = n_plane.dot(p_position - o_point_) / n_plane.dot(v_point_);
 
         // e
-        double e = h - m_;
+        double e = h - t_point_;
 
         // error
         gtsam::Vector error(1);
         error << e;
-        
+
+        // Compute Jacobians
+        if (H_distance) 
+        {
+            // "e" = n_plane.dot(o_plane_ + v_plane_ * "_t_plane" - o_point_) / n_plane.dot(v_point_) - t_point_
+            double d_e__d_t_plane = n_plane.dot(v_plane_) / n_plane.dot(v_point_);
+
+            // fill in the Jacobian
+            *H_distance = gtsam::Matrix(1, 1);
+            (*H_distance)(0, 0) = d_e__d_t_plane;
+        }
+
+        if (H_normal) 
+        {
+            // "e" = "n_plane".dot(p_position - o_point_) / "n_plane".dot(v_point_) - t_point_
+            double v = n_plane.dot(v_point_);
+            double u = n_plane.dot(p_position - o_point_);
+            Eigen::Vector3d d_v__d_n_plane = v_point_;
+            Eigen::Vector3d d_u__d_n_plane = p_position - o_point_;
+            Eigen::Vector3d d_e__d_n_plane = (v * d_u__d_n_plane - u * d_v__d_n_plane) / (v * v);
+
+            // fill in the Jacobian
+            *H_normal = gtsam::Matrix(1, 2);
+            (*H_normal).block<1, 2>(0, 0) = d_e__d_n_plane.transpose() * _n_plane.basis();
+        }
+
         return error;
     }
 };
@@ -74,27 +86,34 @@ int main()
 {
     // Create an empty nonlinear factor graph
     gtsam::NonlinearFactorGraph graph;
-
-    // Prior factor for origin
-    auto priorNoise = gtsam::noiseModel::Diagonal::Sigmas(gtsam::Vector6::Ones() * 0.1);
-    graph.add(gtsam::PriorFactor<gtsam::Pose3>(1, gtsam::Pose3(), priorNoise));
     
-    // Bearing range factor
-    auto bearing_range_noise = gtsam::noiseModel::Diagonal::Sigmas(gtsam::Vector3(0.01, 0.01, 1.0));  // Azimuth, Elevation, Range
-    gtsam::Unit3 bearing = gtsam::Unit3::FromPoint3(gtsam::Point3(0, 0, 1));
-    double range = 1;
-    graph.add(boost::make_shared<gtsam::BearingRangeFactor<gtsam::Pose3, gtsam::Pose3>>(1, 2, bearing, range, bearing_range_noise));
+    // MEASUREMENT
+    Eigen::Vector3d x1_origin(0, 0, 0); Eigen::Vector3d x1_position(0, 0, 1);
+    Eigen::Vector3d x2_origin(0, 0, 0); Eigen::Vector3d x2_position(1, 0, 1);
+    Eigen::Vector3d x3_origin(0, 0, 0); Eigen::Vector3d x3_position(0, 1, 1);
 
-    // Measurement factors
-    gtsam::noiseModel::Diagonal::shared_ptr projection_noise = gtsam::noiseModel::Diagonal::Sigmas((gtsam::Vector(1) << 0.01).finished());
-    graph.add(boost::make_shared<ProjectionFactor>(2, Eigen::Vector3d(0, 0, 0), Eigen::Vector3d(1, 1, 1), projection_noise));
-    graph.add(boost::make_shared<ProjectionFactor>(2, Eigen::Vector3d(0, 0, 0), Eigen::Vector3d(1, 0, 1), projection_noise));
-    graph.add(boost::make_shared<ProjectionFactor>(2, Eigen::Vector3d(0, 0, 0), Eigen::Vector3d(0, 1, 1), projection_noise));
-
-    // Create (deliberately inaccurate) initial estimate
+    // PLANE
+    // key
+    gtsam::Symbol KEY_p_distance('p', 1);
+    gtsam::Symbol KEY_p_normal('p', 2);
+    // sample point to use
+    Eigen::Vector3d x0_origin = x1_origin;
+    Eigen::Vector3d x0_position = x1_position;
+    // contraint
+    Eigen::Vector3d p_origin = x0_origin;
+    Eigen::Vector3d p_direction = (x0_position - x0_origin).normalized();
+    // initial guess
     gtsam::Values initial;
-    initial.insert(1, gtsam::Pose3(gtsam::Rot3::RzRyRx(0.0, 0.0, 0.0), gtsam::Point3(0.0, 0.0, 0.0)));
-    initial.insert(2, gtsam::Pose3(gtsam::Rot3::RzRyRx(0.1, 0.1, 0.1), gtsam::Point3(0.5, 0.0, 0.2)));
+    gtsam::Unit3 initial_normal(1, 1, 1);
+    double initial_distance = (x0_position - x0_origin).norm();
+    initial.insert(KEY_p_distance, initial_distance);
+    initial.insert(KEY_p_normal, initial_normal);
+
+    // FACTORS
+    gtsam::noiseModel::Diagonal::shared_ptr range_noise = gtsam::noiseModel::Diagonal::Sigmas((gtsam::Vector(1) << 0.01).finished());
+    graph.add(boost::make_shared<ProjectionFactor>(KEY_p_distance, KEY_p_normal, p_origin, p_direction, x1_origin, x1_position, range_noise));
+    graph.add(boost::make_shared<ProjectionFactor>(KEY_p_distance, KEY_p_normal, p_origin, p_direction, x2_origin, x2_position, range_noise));
+    graph.add(boost::make_shared<ProjectionFactor>(KEY_p_distance, KEY_p_normal, p_origin, p_direction, x3_origin, x3_position, range_noise));
 
     // print graph
     graph.print("Factor graph:\n");
@@ -108,7 +127,8 @@ int main()
     // Query the marginals
     std::cout.precision(2);
     gtsam::Marginals marginals(graph, result);
-    std::cout << "plane covariance:\n" << marginals.marginalCovariance(1) << std::endl;
+    std::cout << "distance covariance:\n" << marginals.marginalCovariance(KEY_p_distance) << std::endl;
+    std::cout << "normal covariance:\n" << marginals.marginalCovariance(KEY_p_normal) << std::endl;
 
     return 0;
 }
