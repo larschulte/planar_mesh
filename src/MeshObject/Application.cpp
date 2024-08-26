a0b9a7f93110259c0033b303323cd7d80bf0f335
jiahao <github@jiahao.wang>
2024-08-26 10:51:36 +0100

    modify simulation noise process to include both precision and accuracy #2

--- src/MeshObject/Application.cpp
+++ src/MeshObject/Application.cpp
@@ -593,7 +593,7 @@ void Application<PointT>::get_sim_data(Eigen::Vector3d& origin, Eigen::Vector3d&
 {
     Simulation sim;
     sim.set_object(settings_.sim_object);
-    sim.set_noise(settings_.noise_std);
+    sim.set_noise(settings_.range_precision, settings_.range_accuracy);
     sim.get_data_pair(origin, position);
 }
 
