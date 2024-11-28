dcf569fe1cda9e1ca05599526fc8c8a193055006
jiahao <github@jiahao.wang>
2024-11-28 12:15:43 +0000

    set to use bagPointT in main for some datasets

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include "MeshObject/Application.hpp"
 #include "MeshObject/InteractiveViewer.hpp"
 
-using InputPointT = VilensPointT;
+#include "point_type/VilensPointT.hpp"
+#include "point_type/BagPointT.hpp"
+
+// using InputPointT = VilensPointT;
+using InputPointT = BagPointT;
 int main()
 {
     std::srand(30); // Fixed seed
